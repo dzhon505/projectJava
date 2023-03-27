381dca87bd8d51a98660b1fa37906ff4a75a0d87
Dzhon <evgenroo@gmail.com>
2023-03-27 23:53:36 +0300

    add hw1

--- HomeWork1/src/Main.java
+++ HomeWork1/src/Main.java
@@ -1,5 +1,117 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
     }
-}
+
+
+
+//    Task_1.
+//    Создайте массив, в котором будут храниться кубы чисел от 1 до 1000.
+//    Затем вводятся 2 числа из этого диапазона. Используя данные из массива найдите их кубы.
+//
+//    Sample Input:
+//
+//            8
+//            11
+//    Sample Output:
+//
+//            512
+//            1331
+    static void task1(){
+        int[] arr = new int[1000];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) Math.pow(i, 3);
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Введите второе число: ");
+        int num2 = scanner.nextInt();
+        System.out.println(num1+" в кубе "+arr[num1]);
+        System.out.println(num2+" в кубе "+arr[num2]);
+    }
+
+//
+//
+//Task_2.
+//        Вводится число n, затем n чисел целых, по одному на каждой строке.
+//        Затем вводится число, на которое нужно умножить все введённые выше числа.
+//        Выведите на экран результат умножения построчно.
+//
+//        Sample Input:
+//
+//        4
+//        1
+//        2
+//        3
+//        5
+//        2
+//        Sample Output:
+//
+//        2
+//        4
+//        6
+//        10
+
+    static void task2(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество чисел: ");
+        int k = scanner.nextInt();
+        int[] arr = new int[k];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Введите число: ");
+            arr[i] = scanner.nextInt();
+        }
+        System.out.print("Введите множитель: ");
+        int m = scanner.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] * m);
+        }
+    }
+
+//
+//
+//Task_3.
+//        Реализовать простой калькулятор (+,-,=,*), только с целыми числами.
+
+
+    static void task3(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число: ");
+        int b = scanner.nextInt();
+        System.out.println("Укажите операцию: ");
+        char operation = scanner.next().charAt(0);
+        switch (operation) {
+
+            case '-':
+                System.out.println(a + " - " + b + " = " + (a - b));
+                break;
+
+            case '+':
+                System.out.println(a + " + " + b + " = " + (a + b));
+                break;
+
+            case '*':
+                System.out.println(a + " * " + b + " = " + (a * b));
+                break;
+
+            case '/':
+                System.out.println(a + " / " + b + " = " + (a / b));
+                break;
+
+        }
+    }}
+//
+//
+//Task_4. *
+//        +Задано уравнение вида q + w = e, q, w, e >= 0. Некоторые цифры могут быть
+//        заменены знаком вопроса, например, 2? + ?5 = 69. Требуется восстановить выражение
+//        до верного равенства. Предложить хотя бы одно решение или сообщить, что его нет.
+
--- seminar1/src/Main.java
+++ seminar1/src/Main.java
@@ -245,25 +245,82 @@ public class Main {
 //}
 
 
+//public static void main(String[] args){
+//    Scanner scanner = new Scanner(System.in);
+//    System.out.print("Размер массива: ");
+//    int n = scanner.nextInt();
+//    int array[] = new int[n];
+//    int sum = 0;
+//    System.out.println("Введите элементы массива: ");
+//    for (int i = 0; i < n; i++) {
+//        array[i] = scanner.nextInt();
+//        if (i % 2 == 0){
+//            sum += array[i];
+//        }
+//    }
+//    System.out.println("Сумма: " + sum);
+//    for (int i = 0; i < n; i++) {
+//        if (i % 2 == 0){
+//            System.out.println(array[i]);
+//        }
+//
+//    }
+//}
+//}
+//
+//
+//
+//Task_7
+//        Дан список. Выведите те его элементы, которые встречаются в списке только один раз.
+//        Элементы нужно выводить в том порядке, в котором они встречаются в списке.
+//
+//        Входные данные
+//        Вводится список чисел. Все числа списка находятся на одной строке.
+//
+//        Выходные данные
+//        Выведите ответ на задачу.
+//
+//        Sample Input:
+//
+//        6
+//        1
+//        2
+//        2
+//        3
+//        3
+//        3
+//        Sample Output:
+//
+//        1
+//
+//        import java.util.Scanner;
+//
+//class MyProgram{
+//    public static void main(String[] args){
+//
+//    }
+//}
+
+
 public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Размер массива: ");
-    int n = scanner.nextInt();
-    int array[] = new int[n];
-    int sum = 0;
-    System.out.println("Введите элементы массива: ");
-    for (int i = 0; i < n; i++) {
-        array[i] = scanner.nextInt();
-        if (i % 2 == 0){
-            sum += array[i];
-        }
+    int size = scanner.nextInt();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++) {
+        arr[i] = scanner.nextInt();
     }
-    System.out.println("Сумма: " + sum);
-    for (int i = 0; i < n; i++) {
-        if (i % 2 == 0){
-            System.out.println(array[i]);
-        }
+    for (int i = 0; i < arr.length; i++) {
+        int count = 0;
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[i] == arr[j]){
+                count++;
+            }
 
+        }
+        if (count == 1){
+            System.out.println(arr[i]);
+        }
     }
+
 }
 }
